142f3120938eb355a167bf5ad38831c76f720694
CF-SJG <141010553+CF-SJG@users.noreply.github.com>
2025-05-23 15:56:53 +0900

    Update ShareRequestRepository.java
    중복신청 방지

--- src/main/java/com/kijy/strengthhub/repository/ShareRequestRepository.java
+++ src/main/java/com/kijy/strengthhub/repository/ShareRequestRepository.java
@@ -8,4 +8,7 @@ import java.util.List;
 public interface ShareRequestRepository extends JpaRepository<ShareRequest, Long> {
     List<ShareRequest> findByMasterId(Long masterId);
     List<ShareRequest> findByUserId(Long userId);
+
+    // ✅ 중복 신청 여부 확인용 메서드 추가
+    boolean existsByUserIdAndMasterId(Long userId, Long masterId);
 }
