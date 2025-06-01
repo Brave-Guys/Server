2ecddaa78b91702e3ea81ca6da17e9ac89f0cd43
CF-SJG <141010553+CF-SJG@users.noreply.github.com>
2025-06-01 16:34:37 +0900

    Update TestController.java
    테스트컨트롤러

--- src/main/java/com/kijy/strengthhub/controller/TestController.java
+++ src/main/java/com/kijy/strengthhub/controller/TestController.java
@@ -1,1 +1,19 @@
+package com.kijy.strengthhub.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("서버 정상 작동 중 (pong)");
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "서버 루트 접속 성공!";
+    }
+}
