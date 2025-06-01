aad1b44a03c88a34326bd6f15ffd685dcb1319bb
CF-SJG <141010553+CF-SJG@users.noreply.github.com>
2025-06-01 22:12:48 +0900

    Update TestController.java
    Test Update

--- src/main/java/com/kijy/strengthhub/controller/TestController.java
+++ src/main/java/com/kijy/strengthhub/controller/TestController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/ping")
